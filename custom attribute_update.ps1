1f7bd74665e325d9a32dd2aff6e9c9dc2c79c5da
ankh2054 <charles.holtzkampf@gmail.com>
2017-11-14 09:50:51 +0000

    added local active directory update

--- custom attribute_update.ps1
+++ custom attribute_update.ps1
@@ -1,1 +1,6 @@
+#OFFICE365
 Import-CSV "c:\temp\Customattribute1.csv" | % {Set-Mailbox -identity $_.LANID -CustomAttribute10 $_.CA1}
+
+
+#Local Active Directory
+Import-CSV c:\temp\users.csv | Foreach { Set-ADUSer -Identity $_.samaccountname -Department $_.Department }
